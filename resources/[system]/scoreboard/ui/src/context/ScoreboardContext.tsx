753a0c2d7f79a49a313ecd74057d3b67813e0b1c
Christopher <onlygamernerd@gmail.com>
2021-03-18 17:38:43 +0100

    update context

--- resources/[system]/scoreboard/ui/src/context/ScoreboardContext.tsx
+++ resources/[system]/scoreboard/ui/src/context/ScoreboardContext.tsx
@@ -6,15 +6,19 @@ export const ScoreboardContext = createContext<Context | undefined>(undefined);
 export const ScoreboardProvider = ({ children }: { children: React.ReactNode }) => {
 	const [visibility, setVisibility] = useState<boolean>(false);
 	const [data, setData] = useState(null);
+	const [players, setPlayers] = useState(null);
 
 	useNuiEvent('CfxScoreboard', 'setVisibility', setVisibility);
 	useNuiEvent('CfxScoreboard', 'setData', setData);
+	useNuiEvent('CfxScoreboard', 'setPlayers', setPlayers)
 
 	const value = {
 		visibility,
 		setVisibility,
 		data,
-		setData
+		setData,
+		players,
+		setPlayers
 	}
 
 	return <ScoreboardContext.Provider value={value}>{children}</ScoreboardContext.Provider>
@@ -25,6 +29,8 @@ interface Context {
 	setVisibility: (show: boolean) => void;
 	data: any[];
 	setData: (show: boolean) => void;
+	players: any[];
+	setPlayers: (players: any) => void;
 }
 
 export const useScoreboard = () => {
