6674b124114f786a7fbde260b27a6ff6e587af08
Christopher <onlygamernerd@gmail.com>
2021-03-19 01:27:48 +0100

    webpack images

--- resources/[system]/scoreboard/ui/webpack/webpack.common.js
+++ resources/[system]/scoreboard/ui/webpack/webpack.common.js
@@ -33,7 +33,41 @@ module.exports = (options) => ({
 				test: /\.css$/,
 				include: /node_modules/,
 				use: ['style-loader', 'css-loader']
-			}
+			},
+			{
+				test: /\.(jpg|png|gif)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							// Inline files smaller than 10 kB
+							limit: 10 * 1024,
+						},
+					},
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							mozjpeg: {
+								enabled: false,
+								// NOTE: mozjpeg is disabled as it causes errors in some Linux environments
+								// Try enabling it in your environment by switching the config to:
+								// enabled: true,
+								// progressive: true,
+							},
+							gifsicle: {
+								interlaced: false,
+							},
+							optipng: {
+								optimizationLevel: 7,
+							},
+							pngquant: {
+								quality: [0.60, 0.90],
+								speed: 4,
+							},
+						},
+					},
+				],
+			},
 		],
 	},
 
